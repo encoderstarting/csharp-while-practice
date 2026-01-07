bd7511bb5bb0346da622e3e41aa5b04a59e88e5f
encoderstarting <112802133+encoderstarting@users.noreply.github.com>
2026-01-07 23:06:35 +0300

    add file password

---
+++ ConsoleApp5/password.cs
@@ -0,0 +1,32 @@
+﻿class password
+{
+    public static void Run()
+    {
+        Console.WriteLine("Нужно ввести пароль, ниже вам предоставиться ввеси его");
+        int count = 0;
+        int password = 0;
+
+        while (password != 1234)
+        {
+            Console.WriteLine("Введите пароль: ");
+            bool number = int.TryParse(Console.ReadLine(), out password);
+            if (!number)
+            {
+                Console.WriteLine("Повторите попытку, введите числа");
+                continue;
+
+            }
+            else
+            {
+                count++;
+            }
+
+
+        }
+        Console.WriteLine("Пароль верный! ");
+        Console.WriteLine("Пароль неверный! Количество попыток: " + count);
+
+
+
+    }
+}
