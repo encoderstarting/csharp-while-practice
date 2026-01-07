4c10fe33e6fb040d46221634958d08e98ea14e26
encoderstarting <112802133+encoderstarting@users.noreply.github.com>
2026-01-07 22:37:53 +0300

    add while

--- ConsoleApp5/Program.cs
+++ ConsoleApp5/Program.cs
@@ -1,2 +1,24 @@
-﻿
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Нужно ввести пароль, ниже вам предоставиться ввеси его");
+int count = 0;
+int password = 0;
+
+while (password != 1234)
+{
+    Console.WriteLine("Введите пароль: ");
+    bool number = int.TryParse(Console.ReadLine(), out password);
+    if (!number)
+    {
+        Console.WriteLine("Повторите попытку, введите числа");
+        continue;
+       
+    }
+    else
+    {
+        count++;
+    }
+    
+    
+}
+Console.WriteLine("Пароль верный! ");
+Console.WriteLine("Пароль неверный! Количество попыток: " + count);
+
