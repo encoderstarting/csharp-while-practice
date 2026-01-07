29963a5476e90b5001dcb3ca37417de7f2394a08
encoderstarting <112802133+encoderstarting@users.noreply.github.com>
2026-01-07 23:04:00 +0300

    add logic if else

---
+++ ConsoleApp5/Menupractic.cs
@@ -0,0 +1,46 @@
+﻿class Menupractic
+{
+    public static void Run()
+    {
+        Console.WriteLine("Выберите цифру: ");
+        while (true)
+        {
+            Console.WriteLine("1 - сказать привет");
+            Console.WriteLine("2 - Показать время");
+            Console.WriteLine("0 - Выход");
+            Console.WriteLine("Выберите пункт: ");
+            if (!int.TryParse(Console.ReadLine(), out int choise))
+            {
+                Console.WriteLine("Введите число!");
+                continue;
+            }
+            if (choise == 1)
+            {
+                Console.WriteLine("Привет");
+            }
+            else if (choise == 2)
+            {
+
+                Console.WriteLine("Текущее время: " + DateTime.Now);
+            }
+            else if(choise == 0) 
+            {
+                break;
+               
+            }
+            else
+            {
+                Console.WriteLine("Такого нет в меню!");
+                continue;
+                
+            }
+
+
+
+
+
+
+        }
+        
+    }
+}
